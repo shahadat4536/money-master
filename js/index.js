16a490f14c12e08447386f94a790f1817bde5192
Shahadat Hossain <shahadathossain4536@gmail.com>
2022-02-17 17:26:45 +0600

    codedone

--- js/index.js
+++ js/index.js
@@ -1,54 +1,95 @@
-document.getElementById('expenses-calculate').addEventListener('click', function () {
+
+function totalBalanceUpdate() {
+
     const foodExpenses = document.getElementById('food-expenses').value;
     const rentExpenses = document.getElementById('rent-expenses').value;
     const clothesExpenses = document.getElementById('clothes-expenses').value;
-    const totalExpensesValue = parseFloat(foodExpenses) + parseFloat(rentExpenses) + parseFloat(clothesExpenses);
+
+
+    if (isNaN(foodExpenses) || isNaN(rentExpenses) || isNaN(clothesExpenses) || foodExpenses < 0 || rentExpenses < 0 || clothesExpenses < 0) {
+
+        alert('Please Enter a Positive Number')
+        return 0;
+    }
+
+
+
+    const totalExpensesValueUpdate = parseFloat(foodExpenses) + parseFloat(rentExpenses) + parseFloat(clothesExpenses);
+    return totalExpensesValueUpdate;
+
+
+}
+
+
+
+function totalExpensesUpdate() {
+
+    const totalExpensesValue = totalBalanceUpdate()
+    // console.log(totalExpensesValue, llll)
     const totalExpensesText = document.getElementById('total-expenses');
     const totalExpenses = totalExpensesText.innerText;
     totalExpensesText.innerText = totalExpensesValue;
     //update balance
-    const incomeValue = document.getElementById('income').value;
+    const incomeValueText = document.getElementById('income');
+    const incomeValue = incomeValueText.value;
     const balanceValue = document.getElementById('balance');
-    const balanceCalculate = parseFloat(incomeValue) - parseFloat(totalExpensesValue);
+    const balanceCalculate = parseFloat(incomeValue) - totalExpensesValue;
     balanceValue.innerText = balanceCalculate;
+    if (incomeValue < totalExpensesValue) {
+
+        balanceValue.innerText = ''
+        return alert('Please input valid amount of money in number format');
+
+    }
 
 
-})
+    return totalExpenses;
+
+}
+
+console.log(totalBalanceUpdate(), 5555)
+
 
 function savingCalculate(incomeValue, savingInput) {
     const savingResult = (incomeValue * savingInput) / 100;
     return savingResult
 };
 
+document.getElementById('expenses-calculate').addEventListener('click', function () {
+
+    totalExpensesUpdate()
+
+
+
+})
 
 document.getElementById('saving-button').addEventListener('click', function () {
-    const foodExpenses = document.getElementById('food-expenses').value;
-    const rentExpenses = document.getElementById('rent-expenses').value;
-    const clothesExpenses = document.getElementById('clothes-expenses').value;
-    const totalExpensesValue = parseFloat(foodExpenses) + parseFloat(rentExpenses) + parseFloat(clothesExpenses);
-    const totalExpensesText = document.getElementById('total-expenses');
-    const totalExpenses = totalExpensesText.innerText;
-    totalExpensesText.innerText = totalExpensesValue;
-    //update balance
+
     const incomeValue = document.getElementById('income').value;
-    const balanceValue = document.getElementById('balance');
-    const balanceCalculate = parseFloat(incomeValue) - parseFloat(totalExpensesValue);
-    balanceValue.innerText = balanceCalculate;
+
+    const totalExpensesValue = totalExpensesUpdate()
 
     const savingInput = document.getElementById('saving-input').value;
-    // const savingInputCalculate = savingCalculate(incomeValue, savingInput);
+
     const savingAmountText = document.getElementById('saving-amount');
     const savingAmount = savingAmountText.innerText;
 
     savingAmountText.innerText = savingCalculate(incomeValue, savingInput);
 
+    const balanceValue = document.getElementById('balance');
 
+    const balanceCalculate = balanceValue.innerText;
 
     const remainingBalanceText = document.getElementById('remaining-balance');
     const remainingBalanceValue = remainingBalanceText.innerText;
 
-    const remainingBalance = balanceValue.innerText - savingAmountText.innerText;
+    const remainingBalance = balanceCalculate - savingAmountText.innerText;
+    if (remainingBalance < 0) {
 
+        remainingBalanceText.innerText = ''
+        return alert('Please input valid amount of money in number format 007');
+
+    }
     remainingBalanceText.innerText = remainingBalance;
 
 
